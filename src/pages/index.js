f7943f9e0d5c80b8cc6a558bb46ee82c4717c6b1
Matthew Cross <mattyc246@gmail.com>
2020-01-22 22:33:47 +0800

    added z-index to fix modal sitting behind hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,18 @@ import styled from "styled-components";
 import HeroImage from "../assets/JWD-Image/01-Home/01.png";
 
 const Hero = styled.section`
+  position: relative;
   width: 100%;
   height: 70vh;
   background-image: url(${props => props.image});
   background-position: center;
   background-size: cover;
+
+  div {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.3);
+  }
 `;
 
 const TestHero = styled.section`
@@ -19,7 +26,9 @@ const TestHero = styled.section`
 
 const IndexPage = () => (
   <Layout>
-    <Hero image={HeroImage}></Hero>
+    <Hero image={HeroImage}>
+      <div></div>
+    </Hero>
   </Layout>
 );
 
