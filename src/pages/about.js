b7818f0310d3fea4020dcef19ef765f67cf6ee70
Matthew Cross <mattyc246@gmail.com>
2020-01-23 19:28:05 +0800

    added fontawesome react package

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Layout from "../components/Layout";
 
 const About = () => (
-  <>
+  <Layout>
     <h1>About Me</h1>
-  </>
+  </Layout>
 );
 
 export default About;
