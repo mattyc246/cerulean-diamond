a8801b948019eb1e9a88a0ef6ff7be1336208878
Matthew Cross <mattyc246@gmail.com>
2020-01-23 22:18:45 +0800

    added layout wrapper to educate and contact page

---
+++ src/pages/educate.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Educate = () => {
+  return (
+    <Layout>
+      <h1>Educate</h1>
+    </Layout>
+  );
+};
+
+export default Educate;
