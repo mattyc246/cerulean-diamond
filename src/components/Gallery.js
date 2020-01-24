74871e42f51619f92bdd6fa1d4f578cc3f2370ff
Matthew Cross <mattyc246@gmail.com>
2020-01-24 15:24:16 +0800

    masonry gallery complete on homepage

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,15 +1,69 @@
 import React from "react";
 import styled from "styled-components";
+// import Masonry from "react-masonry-component";
+import { GALLERYDATA } from "../constants/DiamondData";
 
 const GalleryContent = styled.section`
-  height: 70vh;
   width: 100%;
+`;
+
+const Masonry = styled.article`
+  margin: 1.5em auto;
+  column-count: 3;
+  column-gap: 1em;
+
+  @media (max-width: 840px) {
+    column-count: 2;
+  }
+`;
+
+const MasonryItem = styled.div`
+  display: inline-block;
+  margin: 0 0 1em;
+  width: 100%;
+  position: relative;
+`;
+
+const Caption = styled.div`
+  position: absolute;
+  left: 0px;
+  bottom: 0px;
+  width: 70%;
+  height: 50px;
+  padding: 10px;
+  box-sizing: border-box;
+  background-color: rgba(255, 255, 255, 0.7);
+
+  p {
+    color: #535353;
+    margin: 0;
+
+    @media (max-width: 840px) {
+      font-size: 8px;
+    }
+  }
+
+  @media (max-width: 840px) {
+    height: 40px;
+    width: 80%;
+  }
 `;
 
 const Gallery = () => {
   return (
-    <GalleryContent>
-      <h1>Masonry Gallery</h1>
+    <GalleryContent className="container">
+      <Masonry>
+        {GALLERYDATA.map((item, index) => {
+          return (
+            <MasonryItem key={index}>
+              <img className="w-100" src={item.imageUrl} alt={item.imageAlt} />
+              <Caption>
+                <p>{item.imageCaption}</p>
+              </Caption>
+            </MasonryItem>
+          );
+        })}
+      </Masonry>
     </GalleryContent>
   );
 };
--- src/constants/DiamondData.js
+++ src/constants/DiamondData.js
@@ -2,6 +2,12 @@ import Carat from "../assets/JWD-Image/03-4c/14.png";
 import Color from "../assets/JWD-Image/03-4c/15.png";
 import Clarity from "../assets/JWD-Image/03-4c/16.png";
 import Cut from "../assets/JWD-Image/03-4c/17.png";
+import Necklace from "../assets/JWD-Image/01-Home/03.png";
+import RingClose from "../assets/JWD-Image/01-Home/04.png";
+import RingStack from "../assets/JWD-Image/01-Home/05.png";
+import EarRings from "../assets/JWD-Image/01-Home/06.png";
+import Pendant from "../assets/JWD-Image/01-Home/07.png";
+import GoldRing from "../assets/JWD-Image/01-Home/08.png";
 
 const DATA = [
   {
@@ -30,4 +36,37 @@ const DATA = [
   }
 ];
 
-export default DATA;
+const GALLERYDATA = [
+  {
+    imageUrl: Necklace,
+    imageAlt: "Necklace With Pendant",
+    imageCaption: "Vestibulum Ante Ipsum"
+  },
+  {
+    imageUrl: RingClose,
+    imageAlt: "Close Up Diamond Ring",
+    imageCaption: "Vestibulum Ante Ipsum"
+  },
+  {
+    imageUrl: RingStack,
+    imageAlt: "Rings Stacked on Top",
+    imageCaption: "Ante Ipsum"
+  },
+  {
+    imageUrl: EarRings,
+    imageAlt: "Pair of Earrings",
+    imageCaption: "Miracle Place"
+  },
+  {
+    imageUrl: Pendant,
+    imageAlt: "Diamond Necklace",
+    imageCaption: "Vestibulum Ante Ipsum"
+  },
+  {
+    imageUrl: GoldRing,
+    imageAlt: "Pair of Gold Rings",
+    imageCaption: "Ante Ipsum"
+  }
+];
+
+export { DATA, GALLERYDATA };
